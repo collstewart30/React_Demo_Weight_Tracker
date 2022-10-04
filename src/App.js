2200d5e19fcec2c1bc9d295f75cd3f22e06cda36
Colleen Stewart <collstewart30@gmail.com>
2022-10-03 20:48:54 -0400

    Completed WeightTracker

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 import AddEntryForm from './Components/AddEntry/AddEntryForm';
 import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
+import './App.css';
 
 
 function App() {
@@ -19,10 +20,24 @@ function App() {
   }
   /* instantiating child components below */
   return (
-    <div>
-      <DisplayEntries parentEntries = {entries}/>   
-      <AddEntryForm addNewEntryProperty={addNewEntry}/>
-      <EntriesChartTracker parentEntries = {entries}/>
+    <div className='container-fluid'>
+      <div className='row'>
+        <h3 style={{margin: '1em'}}>Weight
+        <small className= 'text-muted'>Tracker</small></h3>
+        <div className='col-md-6'>
+          <div className='border-box'>
+          <DisplayEntries parentEntries = {entries}/>   
+          </div>
+          <div className='border-box'>
+          <AddEntryForm addNewEntryProperty={addNewEntry}/>
+          </div>
+        </div>
+        <div className='col-md-6'>
+          <div className='border-box'>
+            <EntriesChartTracker parentEntries = {entries}/>
+          </div>
+        </div>
+      </div>
     </div>
   );    /* not setting addNewEntryProperty={addNewEntry()} bc this will set equal to the result of the function. Setting equal to the function reference instead to use as a pointer to call addNewEntryProperty like a function and it will execute addNewEntry function*/
 }
--- src/Components/DisplayEntries/DisplayEntries.jsx
+++ src/Components/DisplayEntries/DisplayEntries.jsx
@@ -6,7 +6,7 @@
 
 const DisplayEntries = (props) => {
     return ( 
-        <table>
+        <table className='table'>
         <thead>
           <tr>
             <th>Entry Number</th>
@@ -17,7 +17,7 @@ const DisplayEntries = (props) => {
         <tbody>
           {props.parentEntries.map((entry, index) =>{
             return (
-              <tr>
+              <tr key={index}>
                 <td>{index + 1}</td>
                 <td>{entry.weight}</td>
                 <td>{entry.date}</td>
